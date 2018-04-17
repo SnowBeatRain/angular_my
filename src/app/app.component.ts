97481a2ba27fdafb82d2ff75977c4e5683d24dbe
snowbeatrain <15093568689@163.com>
2018-04-17 16:40:14 +0800

    首页底部导航路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  ngOnInit() {
+    console.log($(".navlist"))
+    // $(".navlist").click(function (e) {
+    //   $(e.currentTarget).addClass("active") //给点击的元素添加class
+    //   $(e.currentTarget).siblings()//兄弟节点
+    //   $($(e.currentTarget).siblings()).removeClass("active")
+    // })
+  }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Http, Response, URLSearchParams, BaseRequestOptions } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpParams ,HttpErrorResponse} from '@angular/common/http';
+// import { Http, Response, URLSearchParams, BaseRequestOptions } from '@angular/http';
 import { Observable } from 'rxjs';
 import 'rxjs/Rx';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -14,9 +14,21 @@ export class HttpService {
     res._body = jsonResponse
     return res
   }
-  private handleError<T>(operation = 'operation', result?: T) {
-    return console.error(result); // log to console instead
-  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    // return throwError('Something bad happened; please try again later.');
+    return "error"
+  };
   getInfo(params?: any) {
     // return this.http.get(this.heroesUrl + "/api/Home/PCInfo?pageIndex=1&pageSize=100&status=-1")
     return this.http.get(this.heroesUrl + `/api/Home/PCInfo?pageIndex=${params.pageIndex}&pageSize=${params.pageSize}&status=${params.status}`)
@@ -28,7 +40,9 @@ export class HttpService {
       .pipe(
         tap(res => {
           console.log(res)
-        })
+        }),
+        catchError(this.handleError)
       );
+      
   }
 }
